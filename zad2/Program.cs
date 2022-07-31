b7e4eb9d9b36589a8be75c0b254b030c18a7fca7
Ekaterina <artyuhovae@mail.ru>
2022-07-31 12:58:51 +0300

    add zad2

---
+++ zad2/Program.cs
@@ -0,0 +1,31 @@
+﻿//Решение Артема
+int number = new Random().Next(100,1000);
+Console.WriteLine("Число:" + number);
+
+int numberSecondDigit = number/100;
+int numberThirdDigit = number%10;
+
+Console.WriteLine("Число после удаления второй цифры: " + numberSecondDigit + numberThirdDigit);
+
+//решение кирилла
+{
+int number = new Random().Next(100,1000);
+Console.Write("The random number is : ");
+Console.WriteLine(number);
+String stringNum = number.ToString();
+Console.Write("The modified num is : ");
+Console.Write(stringNum[0]);
+Console.WriteLine(stringNum[2]);
+}
+
+{
+int number = new Random().Next(100,1000);
+Console.Write("The random number is : ");
+Console.WriteLine(number);
+
+int num1 = number / 100;
+int num2 = number % 10;
+
+Console.Write("The modified num is : ");
+Console.WriteLine(num1*10 + num2);
+}
