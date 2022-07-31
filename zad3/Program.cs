1e78912aa19bde56b8e6b4862185020c9c780601
Ekaterina <artyuhovae@mail.ru>
2022-07-31 12:57:47 +0300

    add zad3

---
+++ zad3/Program.cs
@@ -0,0 +1,31 @@
+﻿
+//Решение Николая
+string? inputLkneOne = Console.ReadLine();
+string? inputLkneTwo = Console.ReadLine();
+if(inputLkneOne != null && inputLkneTwo !=null){
+int inputNumberOne = int.Parse(inputLkneOne);
+int inputNumberTwo = int.Parse(inputLkneTwo);
+int div = inputNumberTwo%inputNumberOne;
+if(div == 0){
+Console.WriteLine("yes");
+}else{
+Console.WriteLine(div);
+}
+}
+
+//Решение артема
+Console.Write("Enter first number: ");
+string? firstLine = Console.ReadLine();
+Console.Write("Enter second number: ");
+string? secondLine = Console.ReadLine();
+int first = int.Parse(firstLine);
+int second = int.Parse(secondLine);
+
+if (second%first == 0);
+{
+Console.WritLine("Кратное");
+}
+else 
+{
+ Console.WriteLine("Не кратное,”"+ second%first);
+}
